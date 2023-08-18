056166450cf137246f7a10129c683eed7330173c
Eth3R-One <refaythii@gmail.com>
2023-08-18 21:59:56 +0600

    Added readme and finilized

--- README.md
+++ README.md
@@ -1,70 +1,23 @@
-# Getting Started with Create React App
+# Tic Tac Toe
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Description
 
-## Available Scripts
+This is a simple Tic Tac Toe game written in React. It is a two player game, where the players take turns to mark the spaces in a 3x3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
 
-In the project directory, you can run:
+## How to play
 
-### `npm start`
+1. Clone the repository
+2. Run `npm install` to install the dependencies
+3. Run `npm start` to start the application
+4. Open [http://localhost:3000](http://localhost:3000) to view it in the browser
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Demo
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+![Tic Tac Toe Demo](screenshots/1.gif)
 
-### `npm test`
+## Screenshots
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+![Tic Tac Toe Screenshot 1](screenshots/1.png)
+![Tic Tac Toe Screenshot 2](screenshots/2.png)
+![Tic Tac Toe Screenshot 3](screenshots/3.png)
+![Tic Tac Toe Screenshot 4](screenshots/4.png)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,166 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+/**
+ * @ 1. Create a tic tac toe game
+ * @ 2. Create a restart button
+ * @ 3. Create a popup to show the winner
+ * @ 4. Create a new game button in the popup
+ * @ 5. Create a function to check the winner
+ * @ 6. Create a function to check if the game is draw
+ * @ 7. Scoreboard
+ */
+
+function checkWinner(game) {
+  const winningCombinations = [
+    [0, 1, 2], // 0
+    [3, 4, 5], // 1
+    [6, 7, 8], // 2
+    [0, 3, 6], // 3
+    [1, 4, 7], // 4
+    [2, 5, 8], // 5
+    [0, 4, 8], // 6
+    [2, 4, 6], // 7
+  ];
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (
+      game[a].value &&
+      game[a].value === game[b].value &&
+      game[a].value === game[c].value
+    ) {
+      return game[a].value;
+    }
+  }
+  return null;
+}
+
+function checkDraw(game) {
+  for (let i = 0; i < game.length; i++) {
+    if (game[i].value === "") {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkGameOver(game) {
+  if (checkWinner(game)) {
+    return true;
+  }
+  if (checkDraw(game)) {
+    return true;
+  }
+  return false;
+}
+
+function showPopup(message) {
+  const popup = document.querySelector(".popup");
+  const messageElement = document.querySelector("#message");
+  messageElement.textContent = message;
+  popup.classList.remove("hide");
+}
+
+function hidePopup() {
+  const popup = document.querySelector(".popup");
+  popup.classList.add("hide");
+}
+
+// "〇" : "✕"
+export default function App() {
+  const arr = new Array(9);
+  for (let i = 0; i < 9; i++) {
+    arr[i] = {
+      id: i,
+      value: "",
+    };
+  }
+  const [symbol, setSymbol] = useState("✕");
+  const [game, setGame] = useState(arr);
+  const [player1Score, setPlayer1Score] = useState(0);
+  const [player2Score, setPlayer2Score] = useState(0);
+
+  function insertIntoArray(id) {
+    if (game[id].value === "✕" || game[id].value === "o") return;
+    const newArr = game.map((el) => {
+      if (el.id === id) {
+        return { ...el, value: symbol };
+      }
+      return el;
+    });
+    setGame(newArr);
+    setSymbol(symbol === "✕" ? "〇" : "✕");
+    if (checkGameOver(newArr)) {
+      if (checkWinner(newArr)) {
+        showPopup(`${checkWinner(newArr)} wins`);
+        if (checkWinner(newArr) === "✕") {
+          setPlayer1Score(player1Score + 1);
+        }
+        if (checkWinner(newArr) === "〇") {
+          setPlayer2Score(player2Score + 1);
+        }
+      } else {
+        showPopup("Draw");
+      }
+    }
+  }
+  function resetGame() {
+    restartGame();
+    setPlayer1Score(0);
+    setPlayer2Score(0);
+  }
+
+  function restartGame() {
+    setGame(arr);
+    setSymbol("✕");
+    hidePopup();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="container">
+          <div className="scoreboard">
+            <div>
+              <p>Player ✕</p>
+              <span>{player1Score}</span>
+            </div>
+            <div>
+              <p>Player 〇</p>
+              <span>{player2Score}</span>
+            </div>
+          </div>
+          {game.map((el) => (
+            <AddBox
+              click={() => {
+                console.log(el.id);
+                insertIntoArray(el.id);
+              }}
+              key={el.id}
+              id={el.id}
+            >
+              {el.value}
+            </AddBox>
+          ))}
+        </div>
+      </div>
+      <div className="popup hide">
+        <p id="message">Sample Message</p>
+        <button id="new-game" onClick={restartGame}>
+          New Game
+        </button>
+      </div>
+      <button id="reset" onClick={resetGame}>
+        Reset
+      </button>
+    </>
   );
 }
 
-export default App;
+function AddBox({ id, click, children }) {
+  return (
+    <button className="button-option" onClick={click}>
+      {children}
+    </button>
+  );
+}
